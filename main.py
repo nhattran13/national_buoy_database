79268e6abac541f6188d07415529ddfada88d87f
Nhat Tran <nhattran1971@gmail.com>
2026-02-03 22:46:59 -0600

    Create Dockerfile and container for main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from ndbc_api import NdbcApi
+import csv
+
 api = NdbcApi()
-#print(api.station(station_id=44065, as_df=True))
-print(api.get_modes())
-test_station = api.station(station_id=44065, as_df=False)
-data=api.get_data(station_id='44065', mode="stdmet", as_df=True)
+
+stationID = input("Enter station ID: ")
+modes= input(f"Enter mode from available modes {api.get_modes()}: ")
+
+test_station = api.station(station_id=stationID, as_df=False)
+data=api.get_data(station_id=stationID, mode=modes, as_df=True)
 print(data)
+
+data.to_csv(f"{stationID}_{modes}_data.csv", index=False)
